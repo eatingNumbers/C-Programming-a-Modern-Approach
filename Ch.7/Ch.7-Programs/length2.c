0c9bf83e8f86d2338373e25cfcf5a3bf4fada6b0
Dean <dean@Deans-MacBook-Pro.local>
2018-01-15 06:36:30 -0600

    Add example programs.

---
+++ Ch.7/Ch.7-Programs/length2.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int len = 0;
+
+    printf("Enter a message: ");
+    while (getchar() != '\n')
+        len++;
+    printf("You message was %d character(s) long.\n", len);
+
+    return 0;
+}
